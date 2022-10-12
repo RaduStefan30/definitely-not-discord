b71e6a9adf590d5a368b0e1336acf56ddef7959c
Radu Ranzascu <radustefan3010@google.com>
2022-10-12 12:45:29 +0300

    create login and register on the server

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
+
+const { login, register } = require("../controllers/authController");
+const auth = require("../middleware/auth");
+
+const registerSchema = joi.object({
+  email: joi.string().email().required(),
+  username: joi.string().min(3).max(20).required(),
+  password: joi.string().min(4).max(24).required(),
+});
+
+const loginSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().min(4).max(24).required(),
+});
+
+const router = express.Router();
+
+router.post("/register", validator.body(registerSchema), register);
+
+router.post("/login", validator.body(loginSchema), login);
+
+router.get("/test", auth, (req, res) => {
+  res.send(req.user);
+});
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+require("dotenv").config();
+
+const cors = require("cors");
+const http = require("http");
+const mongoose = require("mongoose");
+
+const authRouter = require("./routes/authRoutes");
+
+const PORT = process.env.PORT || process.env.API_PORT;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1/auth", authRouter);
+
+const server = http.createServer(app);
+
+mongoose
+  .connect(process.env.CONNECTION)
+  .then(() =>
+    server.listen(PORT, () => {
+      console.log(
+        `DB connection established successfully. Server is listening on port ${PORT}...`
+      );
+    })
+  )
+  .catch((err) => console.log("Database connection failed. ", error));
